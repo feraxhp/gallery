225a0fe47972dd9c7866d4437d1d4eb20c3b66f1
feraxhp <feraxhp+gh@gmail.com>
2026-08-04 12:30:22 -0500

    feat: add rounded corners to galery show

--- sharedUI/src/commonMain/kotlin/com/feraxhp/gallery/screens/GalleryScreen.kt
+++ sharedUI/src/commonMain/kotlin/com/feraxhp/gallery/screens/GalleryScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.ui.graphics.Color
@@ -88,7 +89,8 @@ fun GalleryScreen(repository: ImageRepository, albumId: String? = null) {
                             contentDescription = image.name,
                             modifier = Modifier
                                 .aspectRatio(1f)
-                                .fillMaxWidth(),
+                                .fillMaxWidth()
+                                .clip(MaterialTheme.shapes.medium),
                             contentScale = ContentScale.Crop
                         )
 

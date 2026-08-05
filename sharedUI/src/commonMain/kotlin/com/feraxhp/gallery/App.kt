1e7bb28feba0f35baa875afad7fac79a0a8f8182
feraxhp <feraxhp+gh@gmail.com>
2026-08-05 13:45:24 -0500

    feat: add small gradient bellow the toolbar

--- sharedUI/src/commonMain/kotlin/com/feraxhp/gallery/App.kt
+++ sharedUI/src/commonMain/kotlin/com/feraxhp/gallery/App.kt
@@ -26,6 +26,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -33,8 +34,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
@@ -71,6 +74,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.text.AnnotatedString
@@ -252,12 +256,32 @@ fun App(
                                 (currentDestination is Destination.Detail && !isDetailActive)
                         )
 
+                if (showBottomBar && !isActuallyInDetail) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(110.dp)
+                            .align(Alignment.BottomCenter)
+                            .background(
+                                brush = Brush.verticalGradient(
+                                    colors = listOf(
+                                        Color.Transparent,
+                                        MaterialTheme.colorScheme.background.copy(alpha = 0.1f),
+                                        MaterialTheme.colorScheme.background.copy(alpha = 0.5f),
+                                        MaterialTheme.colorScheme.background.copy(alpha = 0.75f)
+                                    )
+                                )
+                            )
+                            .zIndex(0.9f)
+                    )
+                }
+
                 AnimatedVisibility (
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.BottomCenter)
                         .navigationBarsPadding()
-                        .offset(y = -ScreenOffset)
+                        .offset(y = (-8).dp)
                         .zIndex(1f),
                     visible = showBottomBar && !isActuallyInDetail,
                     enter = slideInVertically(

071e8dc90250bae175f65015073b381417faebb2
feraxhp <feraxhp+gh@gmail.com>
2026-09-05 10:20:06 -0500

    chore: update version to 0.1.2

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
         applicationId = "com.feraxhp.gallery.androidApp"
         versionCode = 1
-        versionName = "0.1.1"
+        versionName = "0.1.2"
     }
 
     compileOptions {
